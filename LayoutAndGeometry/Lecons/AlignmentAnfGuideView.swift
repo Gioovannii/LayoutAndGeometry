61062333362254ce49e9ae6f2fe57de7107fdef6
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-17 10:21:34 +0800

    new file for alignment

---
+++ LayoutAndGeometry/Lecons/AlignmentAnfGuideView.swift
@@ -0,0 +1,20 @@
+//
+//  AlignmentAnfGuideView.swift
+//  LayoutAndGeometry
+//
+//  Created by Giovanni Gaffé on 2022/1/17.
+//
+
+import SwiftUI
+
+struct AlignmentAnfGuideView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AlignmentAnfGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlignmentAnfGuideView()
+    }
+}
