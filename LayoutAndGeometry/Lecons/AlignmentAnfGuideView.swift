76904d181a71db992de7310e7e2f22563928a1c8
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-17 10:36:59 +0800

    Set some alignment with closures

--- LayoutAndGeometry/Lecons/AlignmentAnfGuideView.swift
+++ LayoutAndGeometry/Lecons/AlignmentAnfGuideView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct AlignmentAnfGuideView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Numbers \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        Double(position) * -10
+                    }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
@@ -18,3 +28,23 @@ struct AlignmentAnfGuideView_Previews: PreviewProvider {
         AlignmentAnfGuideView()
     }
 }
+
+//Text("Hello world!")
+//    .alignmentGuide(.leading) { d in
+//        d[.trailing]
+//    }
+//Text("This is a longer line of text")
+
+//HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+
+     
+//        Text("Live long and prosper!")
+//            .frame(width: 300, height: 300, alignment: .topLeading)
