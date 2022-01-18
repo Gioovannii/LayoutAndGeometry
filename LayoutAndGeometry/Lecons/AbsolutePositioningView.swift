d6b64fe56acd361da5cde2649a6ed496bfeab17c
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-18 10:06:21 +0800

    add file for absolute position

---
+++ LayoutAndGeometry/Lecons/AbsolutePositioningView.swift
@@ -0,0 +1,20 @@
+//
+//  AbsolutePositioningView.swift
+//  LayoutAndGeometry
+//
+//  Created by Giovanni Gaffé on 2022/1/18.
+//
+
+import SwiftUI
+
+struct AbsolutePositioningView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AbsolutePositioningView_Previews: PreviewProvider {
+    static var previews: some View {
+        AbsolutePositioningView()
+    }
+}
